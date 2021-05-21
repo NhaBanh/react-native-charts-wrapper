782f242d0ba42f1d650857fa6e8901c1a10b9527
nha <nhabanh@itrvn.com>
2021-05-21 11:59:07 +0700

    0.5.78

--- ios/ReactNativeCharts/candlestick/custom/CustomLineScatterCandleRadarRenderer.swift
+++ ios/ReactNativeCharts/candlestick/custom/CustomLineScatterCandleRadarRenderer.swift
@@ -14,6 +14,7 @@
 
 import Foundation
 import CoreGraphics
+import Charts
 
 @objc(CustomLineScatterCandleRadarRenderer)
 open class CustomLineScatterCandleRadarRenderer: CustomBarLineScatterCandleBubbleRenderer
